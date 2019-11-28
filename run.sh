d136983123f30fff272c54c9c737ea564137f62c
jiandongliru <976399651@qq.com>
2019-11-28 20:11:14 +0800

    s

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-echo 'starting heluo project'
+echo 'starting hello project'
 cd /usr/src/heluo
 git pull
 pip install -r venv.txt
 cd /usr/src/heluo/hello
-gunicorn -w 1 -b 0.0.0.0:8000 heluo.wsgi
+gunicorn -w 1 -b 0.0.0.0:8000 hello.wsgi
